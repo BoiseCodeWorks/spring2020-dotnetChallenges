054f6027c4fb9fd5216dc9b3e1a7a8f601098f8c
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-03-23 10:04:37 -0600

    adds loggedin

--- models/User.cs
+++ models/User.cs
@@ -4,16 +4,22 @@ namespace challenges.Models
     {
         public string UserName { get; set; }
         private string _password { get; set; }
+        public bool LoggedIn { get; private set; }
 
         public bool ValidatePassword(string input)
         {
-            return _password == input;
+            if (_password == input)
+            {
+                LoggedIn = true;
+            }
+            return LoggedIn;
         }
 
         public User(string username, string password)
         {
             UserName = username;
             _password = password;
+            LoggedIn = false;
         }
     }
 }
